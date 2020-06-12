fe4d88b668497226adbe63c70d0470aafbcf8c07
Gennady <ltrg67@gmail.com>
2020-06-12 16:11:05 +0300

    Add Demo Project

---
+++ TinyFactory.Demo/Factory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinyFactory.Demo
+{
+    public class Factory : TinyFactory
+    {
+        private static Factory _instace;
+        private static object locker = new object();
+        private  Factory()
+        {
+            Register<RandomNumber>();
+            Singlton(new UserClass("John"));
+        }
+
+        private static Factory GetFactory()
+        {
+            lock(locker)
+            {
+                if (_instace == null) _instace = new Factory();
+                return _instace;
+            }
+        }
+
+        public static T Resolve<T>() where T: class => GetFactory().Get<T>();
+    }
+}
---
+++ TinyFactory.Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TinyFactory.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = Factory.Resolve<UserClass>();
+            Console.WriteLine($"User name: {user.Name}");
+
+            for(int i=0; i< 5; i++)
+            {
+                var number = Factory.Resolve<RandomNumber>();
+                Console.WriteLine(number.Number);
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
