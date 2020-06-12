6e3ef9d8d7729896482c126048bd01ca2846d45e
Gennady <ltrg67@gmail.com>
2020-06-12 12:19:24 +0300

    Add comments

--- TinyFactory/ContainerValue.cs
+++ TinyFactory/ContainerValue.cs
@@ -4,10 +4,22 @@ using System.Text;
 
 namespace TinyFactory
 {
+    /// <summary>
+    /// Container with type and instance reference
+    /// </summary>
     internal class ContainerValue : IDisposable
     {
+        /// <summary>
+        /// Factory registered class type
+        /// </summary>
         internal Type ValueType = null;
+        /// <summary>
+        /// Reference of instance 
+        /// </summary>
         internal object RawValue = null;
+        /// <summary>
+        /// Instance recreation flag
+        /// </summary>
         internal bool Rebuild = false;
 
         public void Dispose()
@@ -17,7 +29,11 @@ namespace TinyFactory
                 todispose?.Dispose();
             RawValue = null;
         }
-
+        /// <summary>
+        /// Get instance from container
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         internal T GetValue<T>()
         {
             if (Rebuild && RawValue != null)
--- TinyFactory/TinyFactory.cs
+++ TinyFactory/TinyFactory.cs
@@ -3,10 +3,21 @@ using System.Collections.Concurrent;
 
 namespace TinyFactory
 {
+    /// <summary>
+    ///  
+    /// </summary>
     public class TinyFactory : IDisposable
     {
+        /// <summary>
+        /// Dictionary with types and instances
+        /// </summary>
         private ConcurrentDictionary<Type, ContainerValue> values = new ConcurrentDictionary<Type, ContainerValue>();
-
+        /// <summary>
+        /// Puts objects in a dictionary
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="obj"></param>
+        /// <param name="rebuild"></param>
         private void AddToValues(Type type, object obj, bool rebuild)
         {
             var value = new ContainerValue()
@@ -21,19 +32,28 @@ namespace TinyFactory
                 throw new Exception("Failed to register type");
             }
         }
-
+        /// <summary>
+        /// Checks for type presence in a dictionary
+        /// </summary>
+        /// <param name="type"></param>
         private void ContainsType(Type type)
         {
             if (values.ContainsKey(type))
                 throw new Exception("This class type has been registered to the factory before");
         }
-
+        /// <summary>
+        /// Checks for a public constructor
+        /// </summary>
+        /// <param name="type"></param>
         private void ConstructorExist(Type type)
         {
             if (type.GetConstructor(Type.EmptyTypes) == null)
                 throw new Exception("The class type must contain an empty public constructor or using Singlton<T>(T value)");
         }
-
+        /// <summary>
+        /// Registers a class type. This type of class will be recreated with every resolve.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
         protected void Register<T>() where T : class
         {
             var insert_type = typeof(T);
@@ -41,7 +61,10 @@ namespace TinyFactory
             ConstructorExist(insert_type);
             AddToValues(insert_type, null, true);
         }
-
+        /// <summary>
+        ///  Registers a class type as singlton
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
         protected void Singlton<T>() where T : class
         {
             var insert_type = typeof(T);
@@ -49,7 +72,11 @@ namespace TinyFactory
             ConstructorExist(insert_type);
             AddToValues(insert_type, null, false);
         }
-
+        /// <summary>
+        /// Registers object as singleton
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
         protected void Singlton<T>(T value) where T : class
         {
             var insert_type = typeof(T);
@@ -58,7 +85,10 @@ namespace TinyFactory
                 throw new ArgumentNullException("Singlton value cannot contain null");
             AddToValues(insert_type, value, false);
         }
-
+        /// <summary>
+        /// Removes a type or instance from the factory.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
         protected void Remove<T>()
         {
             var remove_type = typeof(T);
@@ -70,7 +100,11 @@ namespace TinyFactory
                 GC.WaitForPendingFinalizers();
             }
         }
-
+        /// <summary>
+        /// Get type instance from factory
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
         public T Get<T>() where T : class
         {
             if (values.TryGetValue(typeof(T), out var container))
