280a59f7033049a72871c6c90c40f7a54bd49d48
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-08-19 16:24:00 +0300

    added ServiceDescriptor extension

---
+++ TinyFactory/ServiceDescriptorExt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinyFactory
+{
+    internal static class ServiceDescriptorExt
+    {
+        public static T Resolve<T>(this ServiceDescriptor descriptor, IFactoryProvider provider) where T: class
+        {
+
+
+
+
+            return null;
+        }
+    }
+}
