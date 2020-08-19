18126e0d2d7cdac6c31e0ea0a4c7cf2bbc117b4c
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-08-19 19:01:17 +0300

    added method implementation

--- TinyFactory/ServiceDescriptorExt.cs
+++ TinyFactory/ServiceDescriptorExt.cs
@@ -1,18 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace TinyFactory
 {
     internal static class ServiceDescriptorExt
     {
-        public static T Resolve<T>(this ServiceDescriptor descriptor, IFactoryProvider provider) where T: class
+        public static object Resolve(this ServiceDescriptor descriptor, IFactoryProvider provider)
         {
+            switch (descriptor.Lifetime)
+            {
+                case ServiceLifetime.Transient:
+                    return descriptor.GetInstance(provider);
+                case ServiceLifetime.Singleton:
+                    return descriptor.ImplementationInstance ??
+                        (descriptor.ImplementationInstance = descriptor.GetInstance(provider));
+                default:
+                    return null;
+            }
+        }
 
+        private static object GetInstance(this ServiceDescriptor descriptor, IFactoryProvider provider)
+        {
+            ConstructorInfo item = descriptor.ImplementationType.GetConstructors().FirstOrDefault(o => o.IsPublic && !o.IsStatic);
+            if(item != null)
+            {
+                List<object> args = new List<object>();
+                foreach(var attr in item.GetParameters())
+                {
+                    var attr_type = attr.ParameterType;
+                    args.Add(provider.Get(attr_type) ?? attr.DefaultValue);
+                }
 
-
-
-            return null;
+                return args.Count > 0 ?
+                    Activator.CreateInstance(descriptor.ImplementationType, args.ToArray()) :
+                    Activator.CreateInstance(descriptor.ImplementationType);
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
