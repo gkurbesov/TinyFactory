8d4b4b3047d901236d6073b0a7a1b7016d6ecee9
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-08-19 15:41:43 +0300

    impl of collection

---
+++ TinyFactory/FactoryCollection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinyFactory
+{
+    class FactoryCollection : IFactoryCollection
+    {
+        private List<ServiceDescriptor> collection = new List<ServiceDescriptor>();
+        private bool IsBuild = false;
+
+        public int Count => collection.Count;
+        public bool IsReadOnly => IsBuild;
+
+
+        public IFactoryCollection Build()
+        {
+            IsBuild = true;
+            return this;
+        }
+
+        public void Add(ServiceDescriptor item)
+        {
+            if (!IsReadOnly)
+                collection.Add(item);
+        }
+
+        public void Clear()
+        {
+            if (!IsReadOnly)
+                collection.Clear();
+        }
+
+        public bool Contains(ServiceDescriptor item) =>
+            collection.Contains(item);
+
+        public void CopyTo(ServiceDescriptor[] array, int arrayIndex) =>
+            collection.CopyTo(array, arrayIndex);
+
+        public IEnumerator<ServiceDescriptor> GetEnumerator() =>
+            collection.GetEnumerator();
+
+        public bool Remove(ServiceDescriptor item) =>
+            !IsReadOnly? collection.Remove(item) : false;
+
+        IEnumerator IEnumerable.GetEnumerator() => 
+            collection.GetEnumerator();
+    }
+}
