1a701b99c8e979758d4d32a114110f79375bae0f
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-08-19 15:19:16 +0300

    Add types of lifetime

---
+++ TinyFactory/ServiceLifetime.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinyFactory
+{
+    public enum ServiceLifetime
+    {
+        Singleton = 0x01,
+        Transient = 0x02,
+        HostedService = 0x03,
+        BroadcastService = 0x04,
+    }
+}
