ae0a05b7cf2292b90e6ddbbaf799a12dd83f5d7c
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-08-20 19:08:54 +0300

    added background hosted service components

---
+++ TinyFactory/IHostedService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TinyFactory
+{
+    public interface IHostedService
+    {
+        Task StartAsync(CancellationToken cancellationToken);
+        Task StopAsync(CancellationToken cancellationToken);
+    }
+}
