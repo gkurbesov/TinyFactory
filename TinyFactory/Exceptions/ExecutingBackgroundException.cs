0bd0a5290d2828bd776c9aa4d9c74d6a40e8a2c7
Геннадий Курбесов <39064929+gkurbesov@users.noreply.github.com>
2020-11-17 12:58:02 +0300

    add some exceptions

---
+++ TinyFactory/Exceptions/ExecutingBackgroundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinyFactory.Exceptions
+{
+    public class ExecutingBackgroundException : Exception
+    {
+        public ExecutingBackgroundException() { }
+        public ExecutingBackgroundException(string message)
+        : base(message) { }
+        public ExecutingBackgroundException(string message, Exception innerException)
+        : base(message, innerException) { }
+    }
+}
---
+++ TinyFactory/Exceptions/FactoryConfigurationException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinyFactory.Exceptions
+{
+    public class FactoryConfigurationException : Exception
+    {
+        public FactoryConfigurationException() { }
+        public FactoryConfigurationException(string message)
+        : base(message) { }
+        public FactoryConfigurationException(string message, Exception innerException)
+        : base(message, innerException) { }
+    }
+}
